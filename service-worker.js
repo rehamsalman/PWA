e0288cbbfe7c10d5d01ed03469157ab0131db99b
rehamsalman <rehamsalman17@gmail.com>
2021-02-21 11:04:17 +0400

    new push

--- service-worker.js
+++ service-worker.js
@@ -18,21 +18,18 @@ self.addEventListener('install',(e)=>{
 
     );
 });
-self.addEventListener('fetch', (e) => {
-console.log('[Service Worker] Fetched resource '+e.request.url);
-});
 
-self.addEventListener('fetch',function (e){
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function (r){
-            return r || fetch(e.request).then(function(response){
-                return caches.open(cacheName).then(function(cache){
-                    cache.put(e.request,response.clone());
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache, 
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
                     return response;
                 });
             });
-            
         })
-
     );
 });
